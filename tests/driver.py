a93d90f13a6007c3600f7d5bf9ce3ca19db0e17a
a-gardner1 <abg010@gmail.com>
2023-10-09 21:52:40 +0000

    Update testing framework to use unittest

--- tests/driver.py
+++ tests/driver.py
@@ -1,21 +1,29 @@
 import os
 import stat
 import shutil
-import subprocess
 import sys
+import unittest
+import licenseheaders
+import tempfile
+from pathlib import Path
 
-def run_file(licenseheaders_path, file_path, extra_args=None):
+TEST_FILE = Path(__file__)
+TEST_DIR = TEST_FILE.parent
+
+
+def run_file(file_path, extra_args=None):
     if (extra_args is None):
         extra_args = []
-    subprocess.check_call(
-        [sys.executable, licenseheaders_path,
-        "-t", "lgpl-v3",
-        "-y", "2012-2014",
-        "-o", "ThisNiceCompany",
-        "-n", "ProjectName",
-        "-u", "http://the.projectname.com",
-        "-f", file_path] + extra_args
-    )
+    # HACK to avoid refactoring elsewhere
+    orig_argv = sys.argv
+    sys.argv = [
+        "licenseheaders", "-t", "lgpl-v3", "-y",
+        "2012-2014", "-o", "ThisNiceCompany", "-n", "ProjectName", "-u",
+        "http://the.projectname.com", "-f", file_path
+    ] + extra_args
+    licenseheaders.main()
+    sys.argv = orig_argv
+
 
 def compare_files(file_name, result_file_path, expected_file_path):
     with open(result_file_path) as f1, open(expected_file_path) as f2:
@@ -24,43 +32,52 @@ def compare_files(file_name, result_file_path, expected_file_path):
                 print(f"File {file_name} content is different from expected")
                 return 1
     result_file_permissions = stat.S_IMODE(os.lstat(result_file_path).st_mode)
-    expected_file_permissions = stat.S_IMODE(os.lstat(expected_file_path).st_mode)
+    expected_file_permissions = stat.S_IMODE(
+        os.lstat(expected_file_path).st_mode)
     if (result_file_permissions != expected_file_permissions):
         return 1
     return 0
 
-def main():
-    input_dir = "input"
-    expected_dir = "expected"
-    result_dir = "result"
-    licenseheaders_path = "../licenseheaders.py"
-    differences = 0
 
-    if not os.path.exists(result_dir):
-        os.makedirs(result_dir)
+class TestLicenseHeaders(unittest.TestCase):
 
-    file_names = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
-    for file_name in file_names:
-        input_file_path = os.path.join(input_dir, file_name)
-        expected_file_path = os.path.join(expected_dir, file_name)
-        result_file_path = os.path.join(result_dir, file_name)
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Create a directory for results.
+        """
+        cls.input_dir = TEST_DIR / "input"
+        cls.expected_dir = TEST_DIR / "expected"
+        cls.result_dir = TEST_DIR / "result"
 
-        shutil.copyfile(input_file_path, result_file_path)
+    def test_licenseheaders(self):
+        """
+        Check that adding licenses to known inputs matches expectations.
+        """
+        with tempfile.TemporaryDirectory() as tmpdir:
+            file_names = [
+                f for f in os.listdir(self.input_dir)
+                if os.path.isfile(os.path.join(self.input_dir, f))
+            ]
+            for file_name in file_names:
+                input_file_path = os.path.join(self.input_dir, file_name)
+                expected_file_path = os.path.join(self.expected_dir, file_name)
+                result_file_path = os.path.join(tmpdir, file_name)
 
-        extra_args = []
-        if not os.access(result_file_path, os.W_OK):
-            extra_args += ['--force-overwrite']
+                shutil.copyfile(input_file_path, result_file_path)
 
-        # Run it twice for identifying of removed comments
-        run_file(licenseheaders_path, result_file_path, extra_args)
-        run_file(licenseheaders_path, result_file_path, extra_args)
+                extra_args = []
+                if not os.access(result_file_path, os.W_OK):
+                    extra_args += ['--force-overwrite']
 
-        differences += compare_files(file_name, result_file_path, expected_file_path)
+                # Run it twice for identifying of removed comments
+                run_file(result_file_path, extra_args)
+                run_file(result_file_path, extra_args)
 
-    if differences:
-        return 1
-    else:
-        return 0
+                difference = compare_files(file_name, result_file_path,
+                                             expected_file_path)
+                self.assertEqual(difference, 0)
+
 
 if __name__ == "__main__":
-    sys.exit(main())
+    unittest.main()
